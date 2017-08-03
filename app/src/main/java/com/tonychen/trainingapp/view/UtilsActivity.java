1ce9991ab498f1da8b4182be1c784a9fea782018
ChenYanrong <chenyanrongtony@gmail.com>
2017-08-03 20:32:59 +0800

    1. 添加小工具条目

---
+++ app/src/main/java/com/tonychen/trainingapp/view/UtilsActivity.java
@@ -0,0 +1,15 @@
+package com.tonychen.trainingapp.view;
+
+import android.os.Bundle;
+
+import com.tonychen.trainingapp.R;
+import com.tonychen.trainingapp.view.interf.BaseTitleActivity;
+
+public class UtilsActivity extends BaseTitleActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utils);
+    }
+}
