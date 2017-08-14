181bc95dfb7887332167f6b02aef03a45c2259d9
YanRong.Chen <yanrong.chen@aispeech.com>
2017-08-14 17:50:30 +0800

    1. 初步集成ijkplayer的库,可以正常播放视频

--- app/src/main/java/com/tonychen/trainingapp/view/itemact/IjkPlayerActivity.java
+++ app/src/main/java/com/tonychen/trainingapp/view/itemact/IjkPlayerActivity.java
@@ -1,10 +1,13 @@
 package com.tonychen.trainingapp.view.itemact;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.tonychen.trainingapp.R;
+import com.tonychen.trainingapp.media.IjkVideoView;
 import com.tonychen.trainingapp.view.base.BaseTitleActivity;
 
+import tv.danmaku.ijk.media.player.IMediaPlayer;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 
@@ -15,6 +18,7 @@ public class IjkPlayerActivity extends BaseTitleActivity {
     private static final String IMAGE_URL = "http://vimg2.ws.126.net/image/snapshot/2016/11/I/M/VC62HMUIM.jpg";
 
     private IjkMediaPlayer mVideoView;
+    private IjkVideoView videoView;
 
 
     @Override
@@ -26,12 +30,18 @@ public class IjkPlayerActivity extends BaseTitleActivity {
     @Override
     protected void initView() {
         super.initView();
-//        mVideoView = (IjkMediaPlayer) findViewById(R.id.video_view);
-//        try {
-//            mVideoView.setVideoURI(Uri.parse(VIDEO_URL));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        mVideoView.start();
+        videoView = (IjkVideoView) findViewById(R.id.ijk_videoview);
+        IjkMediaPlayer.loadLibrariesOnce(null);
+        IjkMediaPlayer.native_profileBegin("libijkplayer.so");
+        videoView.setVideoURI(Uri.parse("http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4"));
+
+        videoView.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(IMediaPlayer mp) {
+                videoView.start();
+            }
+        });
+
+
     }
 }
